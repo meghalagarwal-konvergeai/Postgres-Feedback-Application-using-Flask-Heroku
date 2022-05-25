a469d329d67f97e531df01cb68622049c23e9b86
meghalagarwal-konvergeai <meghal.agarwal@konverge.ai>
2022-05-25 16:06:50 +0530

    Feedback Application

--- app.py
+++ app.py
@@ -1,16 +1,37 @@
 from email import message
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from send_mail import send_mail
 
 app = Flask(__name__)
+
 
 ENV = 'dev'
 
 if ENV == 'dev':
     app.debug = True
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/testdb'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://pgadmin:postgres@localhost/testdb'
 else:
     app.debug = False
     app.config['SQLALCHEMY_DATABASE_URI'] = ''
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Feedback(db.Model):
+    __tablename__ = "feedback"
+    id = db.Column(db.Integer, primary_key = True)
+    customer = db.Column(db.String(200), unique=True)
+    dealer = db.Column(db.String(200))
+    rating = db.Column(db.Integer)
+    comments = db.Column(db.Text())
+
+    def __init__(self, customer, dealer, rating, comments) -> None:
+        self.customer = customer
+        self.dealer = dealer
+        self.rating = rating
+        self.comments = comments
 
 @app.route('/')
 def index():
@@ -26,7 +47,15 @@ def submit():
         # print(f"I {customer} puchased a Lexus vehicle and I would like to give a rating of {rating} to {dealer} with a feedback: {comments}")
         if customer == "" or dealer == "":
             return render_template('index.html', message="Please entered the required fields")
-        return render_template('success.html')
+
+        if db.session.query(Feedback).filter(Feedback.customer == customer).count() == 0:
+            data = Feedback(customer, dealer, rating, comments)
+            db.session.add(data)
+            db.session.commit()
+            send_mail(customer, dealer, rating, comments)
+            return render_template('success.html')
+        else:
+            return render_template('index.html', message="You have already submitted the back earlier")
 
 if __name__ == "__main__":
     app.run()
