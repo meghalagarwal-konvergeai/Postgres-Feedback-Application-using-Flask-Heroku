21e3ac3f1ea71701317b9d830515935b941ff9ea
meghalagarwal-konvergeai <meghal.agarwal@konverge.ai>
2022-05-25 13:12:05 +0530

    Feedback Application

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from email import message
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -6,5 +7,17 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/submit', methods=['POST'])
+def submit():
+    if(request.method == 'POST'):
+        customer = request.form['customer']
+        dealer = request.form['dealer']
+        rating = request.form['rating']
+        comments = request.form['comments']
+        # print(f"I {customer} puchased a Lexus vehicle and I would like to give a rating of {rating} to {dealer} with a feedback: {comments}")
+        if customer == "" or dealer == "":
+            return render_template('index.html', message="Please entered the required fields")
+        return render_template('success.html')
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
