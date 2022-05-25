04e4fab096459e0d7cb5fadc6b73bbde27ebce95
meghalagarwal-konvergeai <meghal.agarwal@konverge.ai>
2022-05-25 16:32:59 +0530

    Feedback Application

--- app.py
+++ app.py
@@ -6,14 +6,14 @@ from send_mail import send_mail
 app = Flask(__name__)
 
 
-ENV = 'dev'
+ENV = 'prod'
 
 if ENV == 'dev':
     app.debug = True
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://pgadmin:postgres@localhost/testdb'
 else:
     app.debug = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = ''
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://kbfkuwlzgkvngl:fc2addf88cdb650dc78177c9be1e37d32682f2376884e9e2751ec194fff65d91@ec2-34-236-94-53.compute-1.amazonaws.com:5432/de3ktov1sehu3c'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
